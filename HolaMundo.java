712b2e85cd488ef5bf89896ebf9aeeb68a3187e8
MFdez <Manuel@ceedcv.com>
2024-11-20 13:01:08 +0100

    Con fotos

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,5 @@
 public class HolaMundo {
   public static void main (String[] args) {
-    System.out.println("Proyecto version3 ON GOING");
+    System.out.println("Proyecto version3 ON GOING, y añado imagenes");
   }
 }
